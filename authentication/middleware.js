8f6245797db1138d4a237a7284d2a8228ff29a19
Todd Sinagra <tsinagra@gmail.com>
2016-11-12 15:27:02 -0500

    console isAuthenticated
    log isAuthenticated

--- authentication/middleware.js
+++ authentication/middleware.js
@@ -1,6 +1,6 @@
 function authenticationMiddleware () {
     return function (req, res, next) {
-        console.log("Auth Req: " + req)
+        console.log("Auth Req: " + req.isAuthenticated())
         if (req.isAuthenticated()) {
           return next()
         }
