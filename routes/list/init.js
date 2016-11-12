d077543095188fbd879646ff1bee6e5d65c222f0
Todd Sinagra <tsinagra@gmail.com>
2016-11-12 07:56:17 -0500

    mongodb
    changes to save, update, delete to mongodb

--- routes/list/init.js
+++ routes/list/init.js
@@ -1,14 +1,8 @@
 const passport = require('passport')
 var validator = require('validator');
 
-var mysql      = require('mysql');
-var con = mysql.createConnection({
-  host     : '10.1.76.25',
-  user     : 'user7KP',
-  password : '4yjqIOWhUtUmwiU6',
-  database : 'sampledb'
-})
-con.connect();
+var monk = require('monk');
+var db = monk('localhost:27017/ourplaces');
 
 function initUser (app) {
     app.get('/', renderLogin)
@@ -39,7 +33,7 @@ function logoutUser(req, res) {
 }
 
 function renderList(req, res) {
-    var username = req.user.username.charAt(0).toUpperCase() + req.user.username.slice(1).toLowerCase();
+    var username = req.user.userName.charAt(0).toUpperCase() + req.user.userName.slice(1).toLowerCase();
     allPlaces(function(err, results) {
         res.render('list', {title: 'List', places: results, username: username });
     });
@@ -86,20 +80,15 @@ function renderDelete(req, res) {
 }
 
 function allPlaces(callback) {
-    var sql = 'SELECT p.*, u.username as created_by_username, u1.username as updated_by_username '
-                + 'FROM places p ' 
-                + 'JOIN users u ON u.id = p.created_by '
-                + 'LEFT JOIN users u1 on u1.id = p.updated_by ' 
-                + 'WHERE is_deleted = false '
-                + 'ORDER BY p.id ASC'
-    con.query(sql, function(err, rows, fields) {
+    var collection = db.get('places');
+    collection.find( { isDeleted: false }, function(err, places) {
         if (err) {
-            console.log('Error while performing Query.');
+            console.log('error fetching allPlaces');
             callback(err, null)
         } else {
-            callback(null, rows)
-        } 
-    })
+            callback(null, places)
+        }
+    });
 }
 
 function createPlace(req, res, next) {
@@ -107,7 +96,7 @@ function createPlace(req, res, next) {
     if (validator.isEmpty(req.body.name)) {
         messages.push('Name cannot be blank')
     }
-    if (!validator.isInt(req.body.rank1, { min: 0, max: 10 }) || !validator.isInt(req.body.rank2, { min: 0, max: 10 })) {
+    if (!validator.isInt(req.body.saraRating, { min: 0, max: 10 }) || !validator.isInt(req.body.toddRating, { min: 0, max: 10 })) {
         messages.push('\nRank must be between 0 and 10')    
     }
     if (messages.length > 0) {
@@ -115,11 +104,18 @@ function createPlace(req, res, next) {
         return
     }
 
-    var place = { name: req.body.name, rank1: req.body.rank1, rank2: req.body.rank2, created_by: req.user.id };
-    con.query('INSERT INTO places SET ?', place, function(err,result){
-        if(err) throw err;
-        res.redirect('/list');
-    });
+    var collection = db.get('places');
+        collection.insert({
+            createdDate: new Date(),
+            createdBy: req.user.userName,
+            name: req.body.name,
+            saraRating: req.body.saraRating,
+            toddRating: req.body.toddRating,
+            isDeleted: false
+        }, function(err, equipment) {
+            if(err) throw err;
+            res.redirect('/list')
+    })
 }
 
 function savePlace(req, res, next) {
@@ -127,7 +123,7 @@ function savePlace(req, res, next) {
     if (validator.isEmpty(req.body.name)) {
         messages.push('Name cannot be blank')
     }
-    if (!validator.isInt(req.body.rank1, { min: 0, max: 10 }) || !validator.isInt(req.body.rank2, { min: 0, max: 10 })) {
+    if (!validator.isInt(req.body.saraRating, { min: 0, max: 10 }) || !validator.isInt(req.body.toddRating, { min: 0, max: 10 })) {
         messages.push('\nRank must be between 0 and 10')    
     }
     if (messages.length > 0) {
@@ -135,41 +131,59 @@ function savePlace(req, res, next) {
         return
     }
 
-    con.query(
-        'UPDATE places set name=?, rank1=?, rank2=?, date_updated=?, updated_by=? WHERE id=?',
-        [req.body.name, req.body.rank1, req.body.rank2, new Date(), req.user.id, req.params.id],
-        function (err, result) {
-            if (err) throw err;
-            console.log('Changed ' + result.changedRows + ' rows');
-            res.redirect('/list')
+    var collection = db.get('places');
+    collection.update({ _id: req.params.id }, 
+    { $set:
+        {
+            updatedDate: new Date(),
+            updatedBy: req.user.userName,
+            name: req.body.name,
+            saraRating: req.body.saraRating,
+            toddRating: req.body.toddRating,        
         }
-    );
+    }, function(err, equipment) {
+        if (err) throw err;
+        res.redirect('/list')
+    });
 } 
 
 function deletePlace(req, res) {
-    con.query(
-        'UPDATE places SET is_deleted=true, deleted_by=? WHERE id=?',
-        [req.user.id, req.params.id],
-        function (err, result) {
-            if (err) throw err;
-            res.redirect('/list')
+    var collection = db.get('places');
+    collection.update({ _id: req.params.id }, 
+    { $set:
+        {
+            updatedDate: new Date(),
+            deletedBy: req.user.userName,
+            isDeleted: true            
         }
-    );
+    }, function(err, equipment) {
+        if (err) throw err;
+        res.redirect('/list')
+    })
 }
 
 function getPlaceById(id, callback) {
-    con.query(
-        'SELECT * FROM places where id = ?',
-        [id],
-        function(err, rows, fields) {
-            if (err) {
-                console.log('Error while performing Query.');
-                callback(err, null)
-            } else {
-                callback(null, rows[0])
-            }
-        } 
-    )
+    var collection = db.get('places');
+    collection.findOne({ _id: id }, function(err, place) {
+        if (err) {
+            console.log('Error while performing Query.');
+            callback(null)
+        } else {
+            callback(null, place)
+        }
+    });
+}
+
+function getUserById(id, callback) {
+    var collection = db.get('users');
+    collection.findOne({ _id: id }, function(err, user) {
+        if (err) {
+            console.log('error getting user by id.');
+            callback(null)
+        } else {
+            callback(null, user)
+        }
+    });
 }
 
 module.exports = initUser
